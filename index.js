d0762f8b737d546c8098b3d943c8b227a943c42f
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-12 13:35:27 +0530

    fixing issue

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var mongourl = 'mongodb://localhost/test';
-
-var express = require('express');
-var app = express();
-
 var mongoose = require('mongoose');
+var express = require('express');
+var token = require('serand-token');
+var auth = require('auth');
+
+var mongourl = 'mongodb://localhost/test';
+var app = express();
+var HTTP_PORT = 3000;
+
+auth = auth(token, {
+    open: [
+        '^(?!\\/apis(\\/|$)).+',
+        '^\/apis\/v\/tokens([\/].*|$)',
+        '^\/apis\/v\/vehicles$'
+    ]
+});
+
 mongoose.connect(mongourl);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
     console.log('connected to mongodb : ' + mongourl);
-    app.use(require('auth')({
-        open: [
-            '^(?!\\/apis(\\/|$)).+',
-            '^\/apis\/v\/tokens([\/].*|$)',
-            '^\/apis\/v\/vehicles$'
-        ]
-    }));
+
+    app.use(auth);
+
     app.use('/apis/v', require('user-service'));
     app.use('/apis/v', require('client-service'));
     app.use('/apis/v', require('vehicle-service'));
@@ -25,6 +32,6 @@ db.once('open', function callback() {
     app.use(express.json());
     app.use(express.urlencoded());
 
-    app.listen(4000);
-    console.log('listening on port 4000');
+    app.listen(HTTP_PORT);
+    console.log('listening on port ' + HTTP_PORT);
 });
