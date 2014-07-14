e368037101f3d6655997d71deb95a565c3f8e94d
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-14 22:42:16 +0530

    updating

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var express = require('express');
-var token = require('serand-token');
 var auth = require('auth');
 
 var mongourl = 'mongodb://localhost/test';
 var app = express();
-var HTTP_PORT = 3000;
+var HTTP_PORT = 4004;
 
-auth = auth(token, {
+auth = auth({
     open: [
         '^(?!\\/apis(\\/|$)).+',
         '^\/apis\/v\/tokens([\/].*|$)',
@@ -24,13 +23,13 @@ db.once('open', function callback() {
 
     app.use(auth);
 
+    app.use(express.json());
+    app.use(express.urlencoded());
+
     app.use('/apis/v', require('user-service'));
     app.use('/apis/v', require('client-service'));
     app.use('/apis/v', require('vehicle-service'));
     app.use('/apis/v', require('token-service'));
-
-    app.use(express.json());
-    app.use(express.urlencoded());
 
     app.listen(HTTP_PORT);
     console.log('listening on port ' + HTTP_PORT);
