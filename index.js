34a4a815b92c2ebb210fe8b81c0a605d289de95a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-10-01 12:34:27 +0530

    updating

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var dust = require('dustjs-linkedin');
 var errors = require('errors');
 
 var domain = 'autos';
-var version = nconf.get('CLIENT_' + domain.toUpperCase());
+var version = nconf.get('CLIENT_' + domain.toUpperCase() + '_INDEX');
 var server = nconf.get('SERVER');
 var cdn = nconf.get('CDN');
 
