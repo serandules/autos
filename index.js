a7caff2369dcdbd30f43b9324f0ca31c1e2ff34e
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-10-31 10:17:38 +0530

    updating

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var log = require('logger')('autos');
 var nconf = require('nconf');
 var bodyParser = require('body-parser');
+var dust = require('dustjs-linkedin');
+var _ = require('lodash');
+
 var locate = require('locate');
 var utils = require('utils');
 var serand = require('serand');
-var dust = require('dustjs-linkedin');
 var errors = require('errors');
 var serandi = require('serandi');
 
@@ -14,60 +16,66 @@ var server = utils.serverUrl();
 var cdn = nconf.get('CDN');
 var googleKey = nconf.get('GOOGLE_KEY');
 
-module.exports = function (ctx) {
-    var router = ctx.router;
+module.exports = function (router, done) {
 
-    router.use(bodyParser.urlencoded({extended: true}));
+  router.use(bodyParser.urlencoded({extended: true}));
 
-    serand.index(domain, version, function (err, boot, index) {
-        if (err) {
-            throw err;
-        }
-        dust.loadSource(dust.compile(index, domain));
-        //index page with embedded oauth tokens
-        router.all('/auth', function (req, res) {
-            var context = {
-                server: server,
-                cdn: cdn,
-                boot: boot,
-                version: version,
-                tid: req.body.tid,
-                username: req.body.username,
-                access: req.body.access_token,
-                expires: req.body.expires_in,
-                refresh: req.body.refresh_token,
-                googleKey: googleKey
-            };
-            //TODO: check caching headers
-            dust.render(domain, context, function (err, index) {
-                if (err) {
-                    log.error('dust:render', err);
-                    return res.pond(errors.serverError());
-                }
-                res.set('Content-Type', 'text/html').status(200).send(index);
-            });
+  serand.index(domain, version, function (err, index) {
+    if (err) {
+      return done(err);
+    }
+    dust.loadSource(dust.compile(index, domain));
+    serand.configs(['boot', 'boot-autos'], function (err, configs) {
+      if (err) {
+        return done(err);
+      }
+      //index page with embedded oauth tokens
+      router.all('/auth', function (req, res) {
+        var context = {
+          cdn: cdn,
+          version: version,
+          googleKey: googleKey,
+          server: server,
+          configs: configs,
+          tid: req.body.tid,
+          username: req.body.username,
+          access: req.body.access_token,
+          expires: req.body.expires_in,
+          refresh: req.body.refresh_token
+        };
+        //TODO: check caching headers
+        dust.render(domain, context, function (err, index) {
+          if (err) {
+            log.error('dust:render', err);
+            return res.pond(errors.serverError());
+          }
+          res.set('Content-Type', 'text/html').status(200).send(index);
         });
+      });
 
-        router.use('/apis/*', serandi.notFound);
+      router.use('/apis/*', serandi.notFound);
 
-        //index page
-        router.all('*', function (req, res) {
-            //TODO: check caching headers
-            var context = {
-                server: server,
-                cdn: cdn,
-                boot: boot,
-                version: version,
-                googleKey: googleKey
-            };
-            //TODO: check caching headers
-            dust.render(domain, context, function (err, index) {
-                if (err) {
-                    log.error('dust:render', err);
-                    return res.pond(errors.serverError());
-                }
-                res.set('Content-Type', 'text/html').status(200).send(index);
-            });
+      //index page
+      router.all('*', function (req, res) {
+        //TODO: check caching headers
+        var context = {
+          cdn: cdn,
+          version: version,
+          googleKey: googleKey,
+          server: server,
+          configs: configs
+        };
+        //TODO: check caching headers
+        dust.render(domain, context, function (err, index) {
+          if (err) {
+            log.error('dust:render', err);
+            return res.pond(errors.serverError());
+          }
+          res.set('Content-Type', 'text/html').status(200).send(index);
         });
+      });
+
+      done();
     });
+  });
 };
